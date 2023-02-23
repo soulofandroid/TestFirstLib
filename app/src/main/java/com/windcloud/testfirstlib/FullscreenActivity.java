0788ffdcf51bb27dde00ec0fe38b07510fc33823
rui.wu@peraglobal.com <wr123456>
2023-02-23 11:35:36 +0800

    测试仓库test

---
+++ app/src/main/java/com/windcloud/testfirstlib/FullscreenActivity.java
@@ -0,0 +1,18 @@
+package com.windcloud.testfirstlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class FullscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+    }
+}
